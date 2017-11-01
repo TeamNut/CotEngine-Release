a1caad5a08edb10fb4970c4b49e0c5ea55fb1d0c
맛카치 <mkachi@naver.com>
2017-11-02 02:48:37 +0900

    Fixed Collision

--- include/asset/CotAssetManager.h
+++ include/asset/CotAssetManager.h
@@ -11,6 +11,7 @@ namespace Cot
 		COT_SINGLETON_H(AssetManager);
 	private:
 		std::unordered_map<string, Asset*> _assets;
+		std::unordered_map<string, wstring> _fontCache;
 
 		Asset* GetAsset(const string& key);
 
@@ -23,9 +24,11 @@ namespace Cot
 		{
 			return static_cast<T>(GetAsset(key));
 		}
+		void ReloadAll();
+
+		void AddFontCache(const string& fontPath);
 
 		bool IsHave(const string& key);
-
 		void DestroyAllAssets();
 
 	};
